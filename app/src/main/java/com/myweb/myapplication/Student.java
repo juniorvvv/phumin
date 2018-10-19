a272eb5b3408821269efe2bf6da62fda823cfd35
juniorvvv <44291142+juniorvvv@users.noreply.github.com>
2018-10-19 18:56:56 +0700

    lab6

---
+++ app/src/main/java/com/myweb/myapplication/Student.java
@@ -0,0 +1,12 @@
+package com.myweb.myapplication;
+
+public class Student {
+    public static final String TABLE_NAME ="students";
+
+    public static final String COLUMN_ID = "std_id";
+    public static final String COLUMN_NAME = "std_name";
+
+    private String id;
+    private String name;
+
+}
